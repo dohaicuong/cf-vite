3865f008b25ac90624bf61edac3341730a410e78
Eric Do <beatyshot@gmail.com>
2023-08-05 16:32:42 +1000

    with backend .env

--- functions/api/[[trpc]].ts
+++ functions/api/[[trpc]].ts
@@ -2,13 +2,15 @@ import { appRouter } from '../../trpc'
 import tRPCPlugin, { FetchCreateContextWithCloudflareEnvFnOptions } from 'cloudflare-pages-plugin-trpc'
 
 export type Env = {
-
+  CLERK_SECRET_KEY: string
 }
 
 export type TRPCContext = ReturnType<typeof createContext>
 
-const createContext = ({ req }: FetchCreateContextWithCloudflareEnvFnOptions<Env>) => {
-  return { req }
+const createContext = (ctx: FetchCreateContextWithCloudflareEnvFnOptions<Env>) => {
+  console.log({ clerk_key: ctx.env.CLERK_SECRET_KEY })
+
+  return { req: ctx.req }
 }
 
 export const onRequest: PagesFunction<Env> = tRPCPlugin<Env>({
